fa4e12a5c1d759ad3322fdef3bcfde13adaa46e1
xdream oldlu <xdream@gmail.com>
2026-09-07 13:16:41 +0800

    upgrade dicebear

--- src/lib/generate-avatar.ts
+++ src/lib/generate-avatar.ts
@@ -1,18 +1,26 @@
-import { croodles, initials } from '@dicebear/collection'
-import { createAvatar } from '@dicebear/core'
+// import { croodles, initials } from '@dicebear/collection'
+import { Avatar, Style } from '@dicebear/core'
+import croodles from '@dicebear/styles/croodles.json' with { type: 'json' }
+import initials from '@dicebear/styles/initials.json' with { type: 'json' }
 
 type AvatarProps = {
 	seed: string
 	variant?: 'initials' | 'croodles'
 }
 
+const initialsStyle = new Style(initials)
+const croodlesStyle = new Style(croodles)
+
 export const generateAvatarUri = ({
 	seed,
 	variant = 'initials',
 }: AvatarProps) => {
-	const avatar = createAvatar(variant === 'initials' ? initials : croodles, {
-		seed,
-	})
+	const avatar = new Avatar(
+		variant === 'initials' ? initialsStyle : croodlesStyle,
+		{
+			seed,
+		},
+	)
 
 	return avatar.toDataUri()
 }
